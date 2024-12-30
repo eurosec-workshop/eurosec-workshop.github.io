07901bde7f38ac7770915e321a0a3f230e646fb2
Ioanna Gemou <ioannagemou@gmail.com>
2024-12-30 15:37:48 +0200

    Add new banner

--- style/random-banner.js
+++ style/random-banner.js
@@ -2,7 +2,8 @@
 const bannerImages = [
     'assets/banner1.jpg',
     'assets/banner2.jpg',
-    'assets/banner3.jpg'
+    'assets/banner3.jpg', 
+    'assets/banner4.jpg',
 ];
 
 // Select the header element
